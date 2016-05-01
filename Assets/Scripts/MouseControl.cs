202526090bf2207ca9f8062b5c0f63d64b23ae8c
Mark Feaver <mark.feaver@xero.com>
2016-05-01 16:09:32 +1200

    Created a base project

---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControl : MonoBehaviour {
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : MonoBehaviour {
+    
+    public GameObject tail;
+    public float idleSpeed = 5.0f;
+    public float sprintSpeed = 10.0f;
+    public float rotateSpeed = 5.0f;
+    
+    private bool isAlive;
+    private Rigidbody rb;
+    
+    void Start () {
+        isAlive = true;
+        rb = GetComponent<Rigidbody> ();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (isAlive) {
+            MoveForward ();
+        }
+    }
+    
+    private void MoveForward () {
+        rb.velocity = transform.forward * idleSpeed;
+    }
+}
